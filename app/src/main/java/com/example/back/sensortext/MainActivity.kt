23e14d69a59a7e712cd499b34fec4b47fb4b5a21
humanteqdev1 <kirill.savelyev@humanteq.io>
2018-04-04 17:22:12 +0300

    Progress

--- app/src/main/java/com/example/back/sensortext/MainActivity.kt
+++ app/src/main/java/com/example/back/sensortext/MainActivity.kt
@@ -3,99 +3,13 @@ package com.example.back.sensortext
 import android.hardware.Sensor
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    private val bitmask7: Long = 0b01111111_00000000_00000000_00000000_00000000_00000000_00000000_00000000
-    private val bitmask6: Long = 0b00000000_11111111_00000000_00000000_00000000_00000000_00000000_00000000
-    private val bitmask5: Long = 0b00000000_00000000_11111111_00000000_00000000_00000000_00000000_00000000
-    private val bitmask4: Long = 0b00000000_00000000_00000000_11111111_00000000_00000000_00000000_00000000
-    private val bitmask3: Long = 0b00000000_00000000_00000000_00000000_11111111_00000000_00000000_00000000
-    private val bitmask2: Long = 0b00000000_00000000_00000000_00000000_00000000_11111111_00000000_00000000
-    private val bitmask1: Long = 0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_00000000
-    private val bitmask0: Long = 0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_11111111
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val telemetry = SensorTelemetry(this)
-        telemetry.start("some-id", Sensor.TYPE_GYROSCOPE)
-        telemetry.stop()
-    }
-
-//    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-//    }
-//
-//    override fun onSensorChanged(event: SensorEvent?) {
-//        event?.let {
-//            val builder = StringBuilder(256)
-//            //For each value in array append its string representation
-//            for (i in 0 until it.values.size) {
-//                //Get bits
-//                val value = it.values[i].toBits().toLong()
-//                Log.e("-- TEST", "\n${it.values[i]} = $value\n")
-//
-//                //Shift to match ascii table
-//                val b3 = (value and bitmask3) shr (8 * 3)
-//                val b2 = (value and bitmask2) shr (8 * 2)
-//                val b1 = (value and bitmask1) shr (8 * 1)
-//                val b0 = (value and bitmask0)
-//
-//                //Append 4 chars
-//                builder.append("${b3.toChar()}${b2.toChar()}${b1.toChar()}${b0.toChar()}")
-//            }
-//
-//            //Shift each byte of timestamp to match ascii table
-//            val b7 = (it.timestamp and bitmask7) shr (8 * 7)
-//            val b6 = (it.timestamp and bitmask6) shr (8 * 6)
-//            val b5 = (it.timestamp and bitmask5) shr (8 * 5)
-//            val b4 = (it.timestamp and bitmask4) shr (8 * 4)
-//            val b3 = (it.timestamp and bitmask3) shr (8 * 3)
-//            val b2 = (it.timestamp and bitmask2) shr (8 * 2)
-//            val b1 = (it.timestamp and bitmask1) shr (8 * 1)
-//            val b0 = (it.timestamp and bitmask0)
-//
-//            //Append 8 chars
-//            builder.append("${b7.toChar()}${b6.toChar()}${b5.toChar()}${b4.toChar()}" +
-//                    "${b3.toChar()}${b2.toChar()}${b1.toChar()}${b0.toChar()}")
-//
-//            Log.e("---TEsT", "TIMESTAMP: ${it.timestamp}")
-//            builder.trimToSize()
-//            decodeBytes(builder.toString())
-////            sensorManager.unregisterListener(this)
-//        }
-//    }
-
-    private fun getBits(str: String, offset: Int): Int {
-        val b3 = str[0 + offset].toInt() shl (8 * 3)
-        val b2 = str[1 + offset].toInt() shl (8 * 2)
-        val b1 = str[2 + offset].toInt() shl (8 * 1)
-        val b0 = str[3 + offset].toInt()
-
-        return 0 or b3 or b2 or b1 or b0
-    }
-
-    private fun getLongBits(str: String, offset: Int): Long {
-        val b19 = str[0 + offset].toLong() shl (8 * 7)
-        val b18 = str[1 + offset].toLong() shl (8 * 6)
-        val b17 = str[2 + offset].toLong() shl (8 * 5)
-        val b16 = str[3 + offset].toLong() shl (8 * 4)
-        val b15 = str[4 + offset].toLong() shl (8 * 3)
-        val b14 = str[5 + offset].toLong() shl (8 * 2)
-        val b13 = str[6 + offset].toLong() shl (8 * 1)
-        val b12 = str[7 + offset].toLong()
-
-        return 0L or b19 or b18 or b17 or b16 or b15 or b14 or b13 or b12
-    }
-
-    private fun decodeBytes(str: String) {
-        Log.e("--DECODING", " $str")
-        val restoredX = Float.fromBits(getBits(str, 0))
-        val restoredY = Float.fromBits(getBits(str, 4))
-        val restoredZ = Float.fromBits(getBits(str, 8))
-        val restoredTimestamp = getLongBits(str, 12)
-
-        Log.e("--- TEST", "$restoredX $restoredY $restoredZ $restoredTimestamp")
+//        SensorTelemetry.start(this, "some-id", arrayListOf(Sensor.TYPE_GYROSCOPE, Sensor.TYPE_ACCELEROMETER))
+        SensorTelemetry.start(this, "some-id", Sensor.TYPE_GYROSCOPE)
     }
 }
--- app/src/main/java/com/example/back/sensortext/TelemetryService.kt
+++ app/src/main/java/com/example/back/sensortext/TelemetryService.kt
@@ -1,16 +1,147 @@
 package com.example.back.sensortext
 
 import android.app.IntentService
+import android.content.Context
 import android.content.Intent
-import com.example.back.sensortext.SensorTelemetry.Companion.SENSOR_TELEMETRY
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import com.example.back.sensortext.SensorTelemetry.Companion.SENSOR_TELEMETRY_SESSION_ID
+import com.example.back.sensortext.SensorTelemetry.Companion.SENSOR_TELEMETRY_TYPE
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
 
 class TelemetryService : IntentService("TelemetryService") {
-    private var sensorTelemetry: SensorTelemetry? = null
-
     override fun onHandleIntent(intent: Intent) {
-        sensorTelemetry = intent.extras.getParcelable(SENSOR_TELEMETRY)
+        val bundle = intent.extras
+        bundle?.let {
+            val sessionId = intent.extras.getString(SENSOR_TELEMETRY_SESSION_ID)
+            val type = intent.extras.getInt(SENSOR_TELEMETRY_TYPE)
 
-        handler.handle(intent.getParcelableExtra(CONFIG_KEY))
+            if (sessionId != null)
+                SensorTelemetry(this).start(sessionId, type, 1000)
+        }
+    }
+}
+
+class SensorTelemetry(private val context: Context) : SensorEventListener {
+    companion object {
+        const val SENSOR_TELEMETRY_SESSION_ID = "SENSOR_TELEMETRY_SESSION_ID"
+        const val SENSOR_TELEMETRY_TYPE = "SENSOR_TELEMETRY_TYPE"
+        //        const val MB_1 = 1 * 1024 * 1024
+        const val MB_1 = 64
+        private const val PACK_SIZE = 14
+        const val limit = MB_1 - PACK_SIZE
+
+        fun start(context: Context, id: String, typeList: List<Int>) {
+            val intent = Intent(context, TelemetryService::class.java)
+            intent.putExtra(SENSOR_TELEMETRY_SESSION_ID, id)
+            typeList.forEach {
+                intent.putExtra(SENSOR_TELEMETRY_TYPE, it)
+                context.startService(intent)
+            }
+        }
+
+        fun start(context: Context, id: String, type: Int) {
+            val intent = Intent(context, TelemetryService::class.java)
+            intent.putExtra(SENSOR_TELEMETRY_SESSION_ID, id)
+            intent.putExtra(SENSOR_TELEMETRY_TYPE, type)
+        }
     }
 
+    private lateinit var sensorManager: SensorManager
+    private var latestTimestamp = System.currentTimeMillis()
+    private val byteBuffer = ByteBuffer.allocate(MB_1)
+    private var id = ""
+
+    fun start(id: String, type: Int, rate: Int) {
+        this.id = id
+        buildHeader(id)
+
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(type)?.let {
+            sensorManager.registerListener(this, it, rate * 1000)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, p1: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (byteBuffer.position() > limit) {
+            sendData()
+        }
+
+        event?.let {
+            if (it.values.size == 3) {
+                //Adding x,y,z data
+                for (i in 0 until it.values.size) {
+//                    Log.e("--- VALUE:", "${it.values[i]}")
+                    byteBuffer.putFloat(it.values[i])
+                }
+
+                val now = System.currentTimeMillis()
+                val delta = (now - latestTimestamp) / 1000
+//                Log.e("--- Delta:", "$now - $latestTimestamp = $delta")
+                latestTimestamp = now
+                //Adding timestamp delta
+                byteBuffer.putShort(delta.toShort())
+            }
+        }
+
+//        parseString()
+    }
+
+//    var offset = 0
+//    private fun parseString() {
+//        val builder = StringBuilder(128)
+//
+//        builder.append("\n\nDECODING\nHeader: ${byteBuffer.getHeader(0..6)}\n")
+//        builder.append("Timestamp: ${byteBuffer.getTimestamp(7)}\n")
+//        builder.append("X: ${byteBuffer.getValue(15 + offset)}\n")
+//        builder.append("Y: ${byteBuffer.getValue(19 + offset)}\n")
+//        builder.append("Z: ${byteBuffer.getValue(23 + offset)}\n")
+//        builder.append("Delta: ${byteBuffer.getShortValue(27 + offset)}\n")
+//        offset+=14
+//
+//        Log.e("--- decoded:", "$builder")
+//    }
+
+    private fun buildHeader(id: String) {
+        byteBuffer.clear()
+
+        //Put session id
+        byteBuffer.put(id.toByteArray())
+
+        //Put initial latestTimestamp
+        latestTimestamp = System.currentTimeMillis()
+        byteBuffer.putLong(latestTimestamp)
+    }
+
+    fun stop() {
+        sensorManager.unregisterListener(this)
+        sendData()
+    }
+
+    fun sendData() {
+        val tempByteBuffer = byteBuffer.array().sliceArray(0..byteBuffer.position())
+        buildHeader(id)
+        //Send tempByteBuffer
+    }
+
+    private fun ByteBuffer.getHeader(range: IntRange): String {
+        return String(this.array().sliceArray(range), Charset.defaultCharset())
+    }
+
+    private fun ByteBuffer.getTimestamp(start: Int): String {
+        return this.getLong(start).toString()
+    }
+
+    private fun ByteBuffer.getValue(start: Int): String {
+        return this.getFloat(start).toString()
+    }
+
+    private fun ByteBuffer.getShortValue(start: Int): String {
+        return this.getShort(start).toString()
+    }
 }
